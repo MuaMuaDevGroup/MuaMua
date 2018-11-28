99703b48c460f98a27f464b8d2809dbbdbcb716f
Unknown <samlangten@outlook.com>
2018-11-28 11:39:33 +0800

    add angular login

--- MusicManagement/frontend/src/navbar.js
+++ MusicManagement/frontend/src/navbar.js
@@ -1,11 +1,23 @@
+import $ from 'jquery'
 import 'angular'
-angular.module('mm-app', []).controller('NavbarController', NavbarController);
-
-
-class NavbarController
-{
-    constructor()
-    {
-        this.Name = "Test";
-    }
-}
+angular.module('mm-app', []).controller('NavbarController', ['$scope', function ($scope) {
+    $scope.loginUsername = "";
+    $scope.loginPassword = "";
+    $scope.login = function () {
+        alert("d");
+        let d = {
+            username: this.loginUsername,
+            password: this.loginPassword
+        };
+        $.post({
+            url: "/api/account/login/",
+            data: d,
+            statusCode: {
+                204: alert("OK")
+            },
+            error: () => {
+                alert("Error");
+            }
+        });
+    };
+}]);
--- MusicManagement/frontend/webpack.config.js
+++ MusicManagement/frontend/webpack.config.js
@@ -9,6 +9,11 @@ module.exports = {
     },
     module: {
         rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader'
+        },
+        {
             test: /\.css$/,
             loader: 'style-loader!css-loader'
         },
