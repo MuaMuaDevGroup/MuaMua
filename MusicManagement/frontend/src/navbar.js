b334fd9491f435c8d97f351a1af0189407758b8b
Unknown <samlangten@outlook.com>
2018-11-28 17:08:09 +0800

    finish login

--- MusicManagement/frontend/src/navbar.js
+++ MusicManagement/frontend/src/navbar.js
@@ -1,23 +1,34 @@
 import $ from 'jquery'
 import 'angular'
-angular.module('mm-app', []).controller('NavbarController', ['$scope', function ($scope) {
+
+angular.module('mm-app', []).controller('NavbarController', ["$http", "$scope", function ($http, $scope) {
     $scope.loginUsername = "";
     $scope.loginPassword = "";
+    $scope.username = "111";
+    $scope.email = "111";
+    $scope.isLogin = false;
     $scope.login = function () {
-        alert("d");
         let d = {
-            username: this.loginUsername,
-            password: this.loginPassword
+            username: $scope.loginUsername,
+            password: $scope.loginPassword
         };
-        $.post({
+        $http({
+            method: "POST",
             url: "/api/account/login/",
-            data: d,
-            statusCode: {
-                204: alert("OK")
-            },
-            error: () => {
-                alert("Error");
-            }
+            data: d
+        }).then(function () {
+            $scope.isLogin = true;
+            $http({
+                method: "GET",
+                url: "/api/account/",
+            }).then(function (response) {
+                $scope.username = response.data.username;
+                $scope.email = response.data.email;
+                $scope.$apply();
+            });
+        }, function () {
+            $("#navbar-login-error-box").show();
+            setTimeout(function () { $("#navbar-login-error-box").hide(); }, 3000);
         });
     };
 }]);
