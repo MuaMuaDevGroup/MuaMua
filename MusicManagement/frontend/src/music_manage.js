7d1c53b8886d4d485b003f2a9b028244e6827b9a
SamLangTen <samlangten@outlook.com>
2018-12-10 21:29:03 +0800

    添加用户编辑和用户密码设置

--- MusicManagement/frontend/src/music_manage.js
+++ MusicManagement/frontend/src/music_manage.js
@@ -263,7 +263,7 @@ angular.module('mm-app').controller('UserManageController', ['$http', '$scope', 
         $http({
             url: "/api/user/",
             method: "GET"
-        }).then(response => { 
+        }).then(response => {
             $scope.users = response.data;
         });
     };
@@ -299,5 +299,50 @@ angular.module('mm-app').controller('UserManageController', ['$http', '$scope', 
 
     };
     //Edit user sections
-    
+    $scope.editUser = null;
+    $scope.toEditUser = (id) => {
+        $http({ url: "/api/user/" + id + "/", method: "GET" }).then(response => {
+            $scope.editUser = response.data;
+            $scope.editUser.isAdminRaw = $scope.editUser.is_staff == true ? "true" : "false";
+        });
+    };
+    $scope.editUserDo = () => {
+        let d = {
+            email: $scope.editUser.email,
+            first_name: $scope.editUser.first_name,
+            last_name: $scope.editUser.last_name,
+            is_admin: $scope.editUser.isAdminRaw = $scope.editUser.isAdminRaw == "true" ? true : false
+        };
+        $http({
+            url: "/api/user/" + $scope.editUser.id + "/",
+            method: "PUT",
+            data: d
+        }).then(response => {
+            $scope.editUser = null;
+            $scope.refreshUser();
+        });
+    };
+    //Edit password sections
+    $scope.editPassId = "";
+    $scope.editPassName = "";
+    $scope.editPassNew = "";
+    $scope.toEditPass = (id, name) => {
+        $scope.editPassId = id;
+        $scope.editPassName = name;
+    };
+    $scope.editPass = () => {
+        let d = {
+            new_password: $scope.editPassNew
+        };
+        $http({
+            url: "/api/user/" + $scope.editPassId + "/password/",
+            method: "PUT",
+            data: d
+        }).then(response => { 
+            $scope.editPassId = "";
+            $scope.editPassName = "";
+            $scope.editPassNew = "";
+            $scope.refreshUser();
+        });
+    };
 }]);
