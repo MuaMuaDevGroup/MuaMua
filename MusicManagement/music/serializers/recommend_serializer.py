9044da90c5759fec40d2141466d3bdf58cab40c4
SamLangTen <samlangten@outlook.com>
2018-12-16 00:22:51 +0800

    修复推荐列表serializer的属性声明问题

--- MusicManagement/music/serializers/recommend_serializer.py
+++ MusicManagement/music/serializers/recommend_serializer.py
@@ -1,19 +1,19 @@
 from rest_framework import serializers
-from music.models import Recommend, Album
+from music.models import Recommend, Playlist
 from django.db import models
 
 
 class RecommendListSerializer(serializers.ModelSerializer):
-    album = serializers.PrimaryKeyRelatedField(
-        allow_null=True, queryset=Album.objects.all())
+    playlist = serializers.PrimaryKeyRelatedField(
+        allow_null=True, queryset=Playlist.objects.all())
     class Meta:
         model = Recommend
         fields = ('id', 'playlist', 'playlist_title', 'description', 'date', 'cover')
 
 
 class RecommendCreateSerializer(serializers.ModelSerializer):
-    album = serializers.PrimaryKeyRelatedField(
-        allow_null=True, queryset=Album.objects.all())
+    playlist = serializers.PrimaryKeyRelatedField(
+        allow_null=True, queryset=Playlist.objects.all())
     class Meta:
         model = Recommend
         fields = ('playlist', 'playlist_title', 'description', 'date', 'cover')
