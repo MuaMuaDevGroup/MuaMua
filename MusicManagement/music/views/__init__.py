f2f103ab6bfe2025a645e970e223492997c70f04
SamLangTen <samlangten@outlook.com>
2018-12-16 23:57:59 +0800

    添加普通用户编辑、删除自己的评论

--- MusicManagement/music/views/__init__.py
+++ MusicManagement/music/views/__init__.py
@@ -4,5 +4,5 @@ from .artist import ArtistView, ArtistDetailView, ArtistPhotoUploadView
 from .album import AlbumView, AlbumDetailView, AlbumCoverUploadView
 from .user import UserDetailChangePasswordView, UserDetailView, UserView
 from .playlist import PlaylistDetailView, PlaylistView, PlaylistDetailOwnershipView, PlaylistPhotoUploadView
-from .comment import CommentListView, CommentDetailView, CommentUserView
+from .comment import CommentListView, CommentDetailView, CommentUserView, CommentDetailUserView
 from .recommend import RecommendView, RecommendUpdateView, RecommendUserView, RecommendMusicView, RecommendAlbumView, RecommendPlaylistView
--- MusicManagement/music/views/comment.py
+++ MusicManagement/music/views/comment.py
@@ -1,7 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView, RetrieveAPIView
 from music.serializers import CommentListSerializer, CommentCreateSerializer, CommentUpdateSerializer, CommentUserCreateSerializer
 from music.models import Comment, Music
 from rest_framework.filters import SearchFilter
@@ -78,3 +78,42 @@ class CommentUserView(ListAPIView):
             return Response(status=status.HTTP_201_CREATED)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
+
+
+class CommentDetailUserView(RetrieveAPIView):
+
+    serializer_class = CommentListSerializer
+    permission_classes = (IsAuthenticated, )
+    filter_backends = ()
+
+    def get_queryset(self):
+        user = self.request.user
+        return Comment.objects.filter(user=user)
+
+    def put(self, request, pk, format=None):
+        # Check Exist
+        if len(Comment.objects.filter(pk=pk)) == 0:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        comment = Comment.objects.get(pk=pk)
+        # Check User belongs
+        if comment.user != request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        # Action
+        serializer = CommentUpdateSerializer(comment, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        # Check Exist
+        if len(Comment.objects.filter(pk=pk)) == 0:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        comment = Comment.objects.get(pk=pk)
+        # Check User belongs
+        if comment.user != request.user:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        # Delete
+        comment.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
