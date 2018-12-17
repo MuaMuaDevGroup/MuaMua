b8cbcf58e235358892e1f9efd33eb48dcf831b6c
SamLangTen <samlangten@outlook.com>
2018-12-17 20:08:15 +0800

    添加迷你播放器html

--- MusicManagement/frontend/src/base.js
+++ MusicManagement/frontend/src/base.js
@@ -2,9 +2,23 @@ import 'angular'
 import 'angular-audio'
 import 'angular-file-upload'
 import 'angular-cookies'
-
+import 'linqjs'
 var app = angular.module('mm-app', ['ngAudio', 'angularFileUpload', 'ngCookies']);
 app.config(["$httpProvider", function ($httpProvider) {
     $httpProvider.defaults.xsrfCookieName = 'csrftoken';
     $httpProvider.defaults.xsrfHeaderName = 'X-CSRFToken';
 }]);
+// Register global music interaction
+app.factory('mmMusic', () => { 
+    return () => {
+        // Set Now Playing music
+        this.setMusicPlaying = (music) => {
+            this._onMusicChanged.forEach(e => {
+                e();
+            });
+        };
+        // Register On Music Change
+        this.registerOnMusicChanged = (func) => this._onMusicChanged.push(func);
+        this._onMusicChanged = [];
+    };
+});
