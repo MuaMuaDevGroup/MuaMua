db1bd098902ef6b2f75d2d445b9a240e1aae67d4
KHao <2427870449@qq.com>
2018-12-21 21:22:30 +0800

    用户歌单创建、修改 、删除、收藏歌单、添加我喜欢的歌曲

--- MusicManagement/music/views/playlist.py
+++ MusicManagement/music/views/playlist.py
@@ -1,5 +1,5 @@
 from rest_framework.views import APIView
-from rest_framework.generics import ListAPIView
+from rest_framework.generics import ListAPIView,RetrieveAPIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.filters import SearchFilter
@@ -7,7 +7,7 @@ from django_filters.rest_framework.backends import DjangoFilterBackend
 from rest_framework.pagination import LimitOffsetPagination
 from rest_framework.parsers import MultiPartParser
 from rest_framework.permissions import IsAuthenticated, IsAdminUser,IsAuthenticatedOrReadOnly
-from music.serializers import PlaylistCreationSerializer, PlaylistDetailSerializer, PlaylistOwnersAddSerializer, PlayListOwnersDeleteSerializer, PlayListOwnersDetailSerializer, PlaylistSerializer, PlaylistUpdateSerializer
+from music.serializers import PlaylistCreationSerializer, PlaylistDetailSerializer, PlaylistOwnersAddSerializer, PlayListOwnersDeleteSerializer, PlayListOwnersDetailSerializer, PlaylistSerializer, PlaylistUpdateSerializer,PlaylistUserCreationSerializer,FavoriteMusicSerializer
 from music.models import Music, Album, Artist, Playlist
 from music.permissions import IsAdminOrReadOnly
 import os,hashlib
@@ -132,36 +132,41 @@ class PlaylistPhotoUploadView(APIView):
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 class PlaylistUserView(ListAPIView):
-    queryset = Playlist.objects.all()
     serializer_class=PlaylistSerializer
     filter_backends=(SearchFilter,)
     pagination_class = LimitOffsetPagination
     SearchFilter=('name')
     permission_classes=(IsAuthenticatedOrReadOnly,)
 
+    def get_queryset(self):
+        user=self.request.user
+        return Playlist.objects.filter(owner=user)
+
     def post(self,request,format=None):
-        serializer=PlaylistCreationSerializer(data=request.data)
+        serializer=PlaylistUserCreationSerializer(data=request.data)
         if serializer.is_valid():
-            serializer.save()
+            playlist = Playlist()
+            playlist.name = request.get("name")
+            playlist.description = request.get("description")
+            playlist.songs.set(request.get("songs"))
+            playlist.save()
             return Response(status=status.HTTP_201_CREATED)
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
-class PlaylistDetailUserView(APIView):
-    permission_classes=(IsAuthenticatedOrReadOnly,)
-    def get(self,request,pk,format=None):
-        playlists=Playlist.objects.filter(pk=pk)
-        if len(playlists)==0:
-            return Response(status=status.HTTP_404_NOT_FOUND)
-        playlist=playlists.get(pk=pk)
-        serializer=PlaylistSerializer(playlist)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+class PlaylistDetailUserView(RetrieveAPIView):
+    serializer_class=PlaylistSerializer
+    permission_classes=(IsAuthenticated,)
+    filter_backends=()
+
+    def get_queryset(self):
+        user=self.request.user
+        return Playlist.objects.filter(owner=user)
 
     def put(self,request,pk,format=None):
-        playlists=Playlist.objects.filter(pk=pk)
-        if len(comments)==0:
+        if len(Playlist.objects.filter(pk=pk))==0:
             return Response(status=status.HTTP_404_NOT_FOUND)
-        playlist=playlists.get(pk=pk)
+        playlist=Playlist.objects.get(pk=pk)
         if playlist.owner!=request.owner:
             return Response(status=status.HTTP_403_FORBIDDEN)
         serializer=PlaylistUpdateSerializer(playlist,data=request.data)
@@ -173,7 +178,7 @@ class PlaylistDetailUserView(APIView):
 
     def delete(self,request,pk,format=None):
         playlists=Playlist.objects.filter(pk=pk)
-        if len(comments)==0:
+        if len(playlists)==0:
             return Response(status=status.HTTP_404_NOT_FOUND)
         playlist=playlists.get(pk=pk)
         if playlist.owner!=request.owner:
@@ -181,15 +186,92 @@ class PlaylistDetailUserView(APIView):
         playlist.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
-class PlaylistCollectionView(APIView):
-    pass
+class PlaylistCollectionView(ListAPIView):
+    serializer_class=PlaylistSerializer
+    filter_backends=(SearchFilter,)
+    pagination_class = LimitOffsetPagination
+    SearchFilter=('name')
+    permission_classes=(IsAuthenticated,)
+
+    def get_queryset(self):
+        user=self.request.user
+        return Playlist.objects.filter(collectors=user)    
+    
+    def post(self,request,format=None):
+        serializer=PlaylistUserCollectionAddSerializer(data=request.data)
+        if serializer.is_valid():
+            playlist=Playlist.objects.get(id=serializer.validated_data["id"])
+            playlist.collecters.add(user=request.user)
+            playlist.sava()
+            return Response(status=status.HTTP_201_CREATED)
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
-class PlaylistCollectionDeleteView(APIView):
-    pass
+
+
+class PlaylistCollectionDeleteView(RetrieveAPIView):
+    serializer_class = PlaylistSerializer
+    permission_classes = (IsAuthenticated, )
+    filter_backends = ()
+
+    def get_queryset(self):
+        user=self.request.user
+        return Playlist.objects.filter(collectors=user) 
+
+    def delete(self, request, pk, format=None):
+        if len(Playlist.objects.filter(pk=pk))==0:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        playlist=Playlist.objects.filter(pk=pk)
+        if len(plylist.filter(collecters=request.user)) == 0:
+            return Response(status=status.HTTP_403_FORBIDDEN)
+        playlist.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+      
+
 
 class FavoriteView(APIView):
-    pass
+    serializer_class=PlaylistSerializer
+    filter_backends=(SearchFilter,)
+    pagination_class = LimitOffsetPagination
+    SearchFilter=('name')
+    permission_classes=(IsAuthenticated,)
+
+    def get_queryset(self):
+        user=self.request.user
+        return Playlist.objects.filter(owner=user)
+
+    def post(self,request,format=None):
+        serializer=FavoriteMusicSerializer(data=request.data)
+        if serializer.is_valid():
+            if len(Playlist.objects.filter(name="我喜欢的歌曲"))==0:
+                playlist=Playlist()
+                playlist.name = "我喜欢的歌曲"
+                playlist.owner=request.user
+                playlist.save()
+            playlist=Playlist.objects.get(name="我喜欢的歌曲")
+            playlist.songs.add(Music.objects.get(id=request.validated_data["id"]))
+            return Response(status=status.HTTP_201_CREATED)   
+        else:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+    
 
 class FavoriteDetailView(APIView):
-    pass
+    serializer_class=PlaylistSerializer
+    filter_backends=(SearchFilter,)
+    pagination_class = LimitOffsetPagination
+    SearchFilter=('name')
+    permission_classes=(IsAuthenticated,)
+
+    def get_queryset(self):
+        user=self.request.user
+        return Playlist.objects.filter(owner=user)
+
+    def delete(self,request,pk,format=None):
+        playlist=Playlist.objects.get(name="我喜欢的歌曲")
+        if len(playlist.songs.filter(pk=pk))==0:
+            return Response(status=status.HTTP_404_NOT_FOUND)
+        playlist.songs.remove(id=pk)
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
