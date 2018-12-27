165a363275d1cb550b8ab573aad69ee5c9b59909
SamLangTen <samlangten@outlook.com>
2018-12-27 23:11:15 +0800

    分割管理页面逻辑

---
+++ MusicManagement/frontend/src/admin_app/manage/recommend.js
@@ -0,0 +1,5 @@
+import 'angular'
+angular.module('mm-app').controller('RecommendController', ['$http', '$scope', 'FileUploader', '$cookies', 'djangoPage', 'mmNotification', ($http, $scope, FileUploader, $cookies, djangoPage, mmNotify) => {
+
+
+}]);
